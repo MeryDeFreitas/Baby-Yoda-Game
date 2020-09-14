83b7f966196c5f72377e2b115901e21a99eabf30
Mery De Freitas <mery.dossantos@AES.COM>
2020-09-13 22:33:26 -0300

    dinosaurio se mueve

--- T-Rex.js
+++ T-Rex.js
@@ -1,10 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     const dino = document.querySelector('.dino')
-    
+
     function control(e) {
         if (e.keyCode === 32) {
-            console.log('pressed')
+            jump()
         } 
       }
       document.addEventListener('keyup', control)
+
+      function jump(){
+        let position = 0
+        let timerId = setInterval(function () {
+            //move down
+            if(position === 150)
+            console.log('down')
+
+            //move up
+            console.log('up')
+            position +=30
+            dino.style.bottom = position + 'px'
+        },20)
+      }
+      //Minuto 10:45
+
     })
