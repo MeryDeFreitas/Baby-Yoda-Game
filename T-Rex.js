a83592afe66e3232261c703196f02a83b7beb48c
Mery De Freitas <mery.dossantos@AES.COM>
2020-09-17 22:00:50 -0300

    Movimientos del dinosaurio

--- T-Rex.js
+++ T-Rex.js
@@ -1,26 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
     const dino = document.querySelector('.dino')
+    let isJumping = false
+    let gravity = 0.9
 
     function control(e) {
         if (e.keyCode === 32) {
+          if(!isJumping){
+            isJumping = true
             jump()
+          }
         } 
       }
+
       document.addEventListener('keyup', control)
 
+      let position = 0
+
       function jump(){
-        let position = 0
+
+        let count = 0
         let timerId = setInterval(function () {
+
             //move down
-            if(position === 150)
+            if(count === 15)
+            clearInterval(timerId)
+            //Este método se utiliza para detener el bucle cronometrado 
+            //que se inició con el método setInterval () anterior.
             console.log('down')
+            let downTimerID = setInterval(function(){
+              if (count === 0){
+                clearInterval(downTimerID)
+                isJumping = false
+              }
+              position -= 30 
+              count --
+              position = position * gravity
+              dino.style.bottom = position + 'px'
+            }, 20)
 
             //move up
             console.log('up')
+            count++
             position +=30
+            position = position * gravity
             dino.style.bottom = position + 'px'
-        },20)
+            console.log(dino.style.bottom)
+            },20)
       }
-      //Minuto 10:45
-
+      
+//Minuto 15:37
     })
