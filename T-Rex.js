83fbfe332f234ce9ff378f7934fa68e7aa8ab5c7
Mery De Freitas <mery.dossantos@AES.COM>
2020-09-17 22:15:08 -0300

    Salto corregido

--- T-Rex.js
+++ T-Rex.js
@@ -1,52 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const dino = document.querySelector('.dino')
-    let isJumping = false
-    let gravity = 0.9
+  const dino = document.querySelector('.dino')
+  let isJumping = false
+  let gravity = 0.9
+  
+  function control(e) {
+    if (e.keyCode === 32) {
+      if (!isJumping) {
+        //Cuando presione tecla 32 el dinosaurio salta
+        isJumping = true
+        jump()
+      }
+    }
+  }
 
-    function control(e) {
-        if (e.keyCode === 32) {
-          if(!isJumping){
-            isJumping = true
-            jump()
+  document.addEventListener('keyup', control)
+  
+  let position = 0
+
+  function jump() {
+
+    let count = 0
+    let timerId = setInterval(function () {
+      //move down
+      if (count === 15) {
+        clearInterval(timerId) //Cancela el setInterval anterior
+        let downTimerId = setInterval(function () {
+          if (count === 0) {
+            clearInterval(downTimerId)
+            isJumping = false
           }
-        } 
+          position -= 5
+          count--
+          position = position * gravity
+          dino.style.bottom = position + 'px'
+        },20)
+  
       }
+      //move up
+      position +=30
+      count++
+      position = position * gravity
+      dino.style.bottom = position + 'px'
+    },20)
+  }
 
-      document.addEventListener('keyup', control)
-
-      let position = 0
-
-      function jump(){
-
-        let count = 0
-        let timerId = setInterval(function () {
-
-            //move down
-            if(count === 15)
-            clearInterval(timerId)
-            //Este método se utiliza para detener el bucle cronometrado 
-            //que se inició con el método setInterval () anterior.
-            console.log('down')
-            let downTimerID = setInterval(function(){
-              if (count === 0){
-                clearInterval(downTimerID)
-                isJumping = false
-              }
-              position -= 30 
-              count --
-              position = position * gravity
-              dino.style.bottom = position + 'px'
-            }, 20)
-
-            //move up
-            console.log('up')
-            count++
-            position +=30
-            position = position * gravity
-            dino.style.bottom = position + 'px'
-            console.log(dino.style.bottom)
-            },20)
-      }
-      
-//Minuto 15:37
-    })
+})
